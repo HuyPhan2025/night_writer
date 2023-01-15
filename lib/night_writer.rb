e9871e8525d094f86965373cbade9d7531839df9
Huy Phan <huyphan520@gmail.com>
2023-01-14 18:28:07 -0600

    Iteration 2 progress

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,21 +1,62 @@
 class NightWriter
   attr_accessor :read_file,
-                :write_file
+                :write_file,
+                :braille_alphabets
 
   def initialize
     @read_file = ARGV[0]
     @write_file = ARGV[1]
+    @braille_alphabets = {
+      "a" => ["0.", "..", ".."],
+      "b" => ["00", "..", ".."],
+      "c" => ["0.", ".0", ".."],
+      "d" => ["0.", ".0", "0."],
+      "e" => ["0.", "..", "0."],
+      "f" => ["00", ".0", ".."],
+      "g" => ["00", ".0", "0."],
+      "h" => ["00", "..", "0."],
+      "i" => [".0", ".0", ".."],
+      "j" => [".0", ".0", "0."],
+      "k" => ["0.", "0.", ".."],
+      "l" => ["00", "0.", ".."],
+      "m" => ["0.", "00", ".."],
+      "n" => ["0.", "00", "0."],
+      "o" => ["0.", "0.", "0."],
+      "p" => ["00", "00", ".."],
+      "q" => ["00", "00", "0."],
+      "r" => ["00", "0.", "0."],
+      "s" => [".0", "00", ".."],
+      "t" => [".0", "00", "0."],
+      "u" => ["0.", "0.", ".0"],
+      "v" => ["00", "0.", ".0"],
+      "w" => [".0", ".0", "00"],
+      "x" => ["0.", "00", ".0"],
+      "y" => ["0.", "00", "00"],
+      "z" => ["0.", "0.", "00"],
+      " " => ["..", "..", ".."]
+    }
   end
   
-  def call
-    english_text = File.open(@read_file, "r")
+  def message_read_write
+    english_text = File.read(@read_file)
     
-    text = english_text.read
+    puts "Created #{@write_file} containing #{english_text.split('').count} characters" 
 
-    puts "Created #{@write_file} containing #{text.split('').count} characters" 
-    File.write(@write_file, text) 
+    translated_text = translates_english_to_braille(english_text)
+
+    File.write(@write_file, english_text) 
   end  
-end
-# night_writer = NightWriter.new
-# night_writer.call
 
+  def translates_english_to_braille(english_text)
+    array_text = english_text.split('')
+
+    array_text.flat_map do |letter|
+      @braille_alphabets[letter]
+    end   
+  end 
+end
+
+# require'pry';binding.pry
+# require'pry';binding.pry
+# night_writer = NightWriter.new
+# night_writer.message_read_write
