93521a544c08b71eba2a30ca0e6e661ee7d5a725
Huy Phan <huyphan520@gmail.com>
2023-01-16 13:02:02 -0600

    create before block

--- spec/language_translator.rb
+++ spec/language_translator.rb
@@ -3,6 +3,13 @@ require_relative 'spec_helper'
 RSpec.describe LanguageTranslator do
   let(:language_translator) {LanguageTranslator.new}
 
+  before do
+    night_writer.read_file = './english_text.txt'
+    night_writer.write_file = './braille_text.txt'
+    night_reader.read_file = './braille_text.txt'
+    night_reader.write_file = './english_text.txt'
+  end
+
   describe '#initialize' do
     it "exists" do
       expect(language_translator).to be_instance_of(LanguageTranslator)
